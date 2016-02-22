c6f25da329159851e464c054f1a37c05b97c0ee3
Jacob H. Garrison <omonoid@uw.edu>
2016-02-22 13:43:43 -0800

    finished 2 and 3

--- hw6/q3.m
+++ hw6/q3.m
@@ -1,9 +1,10 @@
-ws = 2000; wp = 500; As = 50; Rp = 0.25;
 
-[N, Om_c] = buttord(wp, ws, Rp, As, 's')
-[b, a] = u_buttap(N, Om_c);
+Rp = 1; As = 50; Om_p = 10; Om_s = 15
+[N,Omega_c]=cheb1ord(Om_p/100, Om_s/100, Rp, As);
+[b, a]=u_chb1ap(N,Rp, Om_p);
+
 % Calculation of Frequency Response:
-[db,mag,pha,w] = freqs_m(b,a, ws);
+[db,mag,pha,w] = freqs_m(b,a, Om_s);
 
 % Plots
 subplot(4,1,1); plot(mag);title('Magnitude Response')
@@ -15,9 +16,5 @@ xlabel('Frequency (pi)'); ylabel('dB'); ; xlim([0, 500])
 subplot(4,1,3); plot(pha); title('Phase Response')
 xlabel('Frequency (pi)'); ylabel('rad'); ; xlim([0, 500])
 
-subplot(4,1,4); impulse(b,a); title('Impulse Response')
+subplot(4,1,4); impulse(b,a);; title('Impulse Response')
 xlabel('time (s)'); ylabel('ha(t)');
-
-b
-a
-disp('transfer function num and den')
